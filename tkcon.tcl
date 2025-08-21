6b2e20f50029e65391737b03a4d0ff45000e5ad0
Mason M <mason@bandoti.com>
2025-08-20 21:33:28 -0300

    Bump version to 3.0

--- tkcon.tcl
+++ tkcon.tcl
@@ -1,7 +1,7 @@
 #!/usr/bin/env wish
 
 # @@ Meta Begin
-# Application tkcon 2.8
+# Application tkcon 3.0
 # Meta platform     tcl
 # Meta summary      Enhanced Tk Console
 # Meta description  Enhanced Tk Console
@@ -74,7 +74,7 @@ catch {unset pkg file name version}
 namespace eval ::tkcon {
     # when modifying this line, make sure that the auto-upgrade check
     # for version still works.
-    variable VERSION "2.8"
+    variable VERSION "3.0"
     # The OPT variable is an array containing most of the optional
     # info to configure.  COLOR has the color data.
     variable OPT
